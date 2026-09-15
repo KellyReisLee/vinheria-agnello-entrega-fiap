c574dd9d73d0c59cac3a5b406e623f6c6facb6c1
“KellyReisLee” <kristinaalvares28@gmail.com>
2026-09-15 12:08:32 +0100

    teste DB

--- src/main/java/com/gnello/connection/ConnectionFactory.java
+++ src/main/java/com/gnello/connection/ConnectionFactory.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-	private static final String URL = "jdbc:postgresql://db.nllntgowozapqdysntlk.supabase.co:5432/postgres";
-	private static final String USER = "postgres";
+	private static final String URL = System.getenv("SUPABASE_DB_URL");
+	private static final String USER = System.getenv("SUPABASE_DB_USER");
 	// Pega a senha da variável de ambiente do sistema
 	private static final String PASSWORD = System.getenv("SUPABASE_DB_PASSWORD");
 
@@ -17,7 +17,9 @@ public class ConnectionFactory {
 	    } catch (ClassNotFoundException e) {
 	        throw new RuntimeException("Erro: Driver JDBC do PostgreSQL não encontrado nas bibliotecas!", e);
 	    } catch (SQLException e) {
-	        throw new RuntimeException("Erro ao conectar com o banco de dados no Supabase!", e);
+	        // Imprime o erro técnico real no log do Render
+	        e.printStackTrace();
+	        throw new RuntimeException("Erro ao conectar com o banco: " + e.getMessage(), e);
 	    }
 	}
 }
