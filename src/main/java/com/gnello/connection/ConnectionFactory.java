b84e4b858fcd9021d14dec7ba8f9a04d9358d243
“KellyReisLee” <kristinaalvares28@gmail.com>
2026-09-15 14:38:18 +0100

    teste conexao 10

--- src/main/java/com/gnello/connection/ConnectionFactory.java
+++ src/main/java/com/gnello/connection/ConnectionFactory.java
@@ -3,6 +3,7 @@ package com.gnello.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
     
@@ -10,10 +11,15 @@ public class ConnectionFactory {
         try {
             Class.forName("org.postgresql.Driver");
             
-            // URL limpa sem parâmetros problemáticos para o driver Java antigo
-            String url = "jdbc:postgresql://neondb_owner:npg_kJ1bN0ArPGFu@ep-cold-boat-b51gyhxk-pooler.c-7.us-east-2.aws.neon.tech/neondb?sslmode=require";
+            // URL limpa apenas com o endereço e o SSL, sem usuário/senha na string
+            String url = "jdbc:postgresql://ep-cold-boat-b51gyhxk-pooler.c-7.us-east-2.aws.neon.tech/neondb?sslmode=require";
             
-            return DriverManager.getConnection(url);
+            // Passando as credenciais separadamente para evitar o bug do driver antigo
+            Properties props = new Properties();
+            props.setProperty("user", "neondb_owner");
+            props.setProperty("password", "npg_kJ1bN0ArPGFu");
+            
+            return DriverManager.getConnection(url, props);
             
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Erro: Driver JDBC do PostgreSQL não encontrado nas bibliotecas!", e);
